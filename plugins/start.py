92e77c0db9006f0e7e8b87bd4ba7b595988f4254
NeonAn <justasneon@gmail.com>
2025-04-04 17:14:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -237,8 +237,8 @@ async def delete_files(messages, client, k):
 
 
 
-# Jishu Developer 
+# MyselfNeon
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+# Telegram Channel @NeonFiles
+# Second Channel @AnimeZerox
+# Developer @NeonAn23
