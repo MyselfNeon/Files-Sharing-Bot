0b33f848b6214a820581a067b7bd752d61b61ffc
NeonAn <justasneon@gmail.com>
2025-04-04 17:16:46 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,8 +11,8 @@ async def web_server():
 
 
 
-# Jishu Developer 
+# MyselfNeon
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+# Telegram Channel @NeonFiles
+# Second Channel @AnimeZerox
+# Developer @NeonAn23
