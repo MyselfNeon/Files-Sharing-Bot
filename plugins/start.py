7b1d7086adf3177cf6d2b39326084580b4f24449
NeonAn <justasneon@gmail.com>
2025-04-05 20:47:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
                 pass
 
 
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\n<b>Uploaded Video / Files Will Be Deleted In {file_auto_delete} (Due To Copyright Issues).\n\n📌 Forward This Video / Files To Somewhere Else And Start Downloading There 😉</b>")
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\n<b>Files/Videos Will Be Deleted In {file_auto_delete} (Due To Copyright Issues).\n\n📌 Forward This Video/Files To your Saved Messages And Start Downloading There 🕊️</b>")
 
         # Schedule the file deletion
         asyncio.create_task(delete_files(madflix_msgs, client, k))
